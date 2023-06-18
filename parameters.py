c42461140cefc5a2fc4ef9528d2b2131c57a5cc0
Aisu Wata <aisu.wata0@gmail.com>
2023-06-18 17:58:13 -0300

    AParametersTha constructor from key value pairs

--- parameters.py
+++ parameters.py
@@ -1,4 +1,5 @@
 
+from typing import Dict, List
 from AnimationStates.animation import Animation, AParameters
 import numpy as np
 
@@ -19,10 +20,19 @@ class AParametersTha(AParameters):
     pose_parameter_groups = get_pose_parameters().get_pose_parameter_groups()
     pose_parameter_count = pose_parameter_count
 
-    def __init__(self, array: np.ndarray | None = None):
+    def __init__(self, array: np.ndarray | dict[str, float] | list[float] | None = None):
         if array is None:
             array = np.array([0.0] * AParametersTha.pose_parameter_count)
-        self.array = array
+        elif isinstance(array, np.ndarray):
+            self.array = array
+        elif isinstance(array, list):
+            array = np.array(array)
+        elif isinstance(array, dict):
+            self.__init__()
+            for k, v in array.items():
+                self[k] = v
+        else:
+            raise NotImplementedError()
     
     def get_parameter_index(self, pose_parameter_name):
         return AParametersTha.pose_parameters.get_parameter_index(pose_parameter_name)
@@ -47,6 +57,26 @@ class AParametersTha(AParameters):
     def __rmul__(self, scalar: float) -> AParameters:
         return AParametersTha(self.array * scalar)
 
+    @classmethod
+    def from_list(cls, param_list: List[Dict[str, float]|List[float]]):
+        """
+        Animation_tha.states_from_list([
+            {'eyebrow_happy_left': 1.0},
+            {'eyebrow_happy_right': 1.0},
+        ])
+        """
+        s_list = []
+        if len(param_list) == 0:
+            return s_list
+        if (isinstance(param_list[0], dict) or
+            isinstance(param_list[0], list) or
+            isinstance(param_list[0], np.ndarray)
+        ):
+            for s in param_list:
+                s_list.append(AParametersTha(s))
+        else: 
+            raise NotImplementedError()
+        return s_list
 
 
 def model_input_split(model_input_arr, time_counter):
